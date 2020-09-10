6b79fedde514ad8604078efbe0f8d5698383055b
DianaMS <dmartinezs@uni.pe>
2020-09-10 16:39:52 -0500

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,14 @@ import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { ItemComponent } from './pages/item/item.component';
 
 const routes: Routes = [
-  { path: '', component: PortafolioComponent },
+  { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'item', component: ItemComponent },
-  { path: '**', pathMatch: 'full', redirectTo: ''}
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
